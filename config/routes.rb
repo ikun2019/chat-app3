bc5a86dd2168bb3508d33dd4988b7d684a1bf261
ikun <isao.miyagawa.2019@gmail.com>
2021-01-03 23:00:12 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create] do
-    resources :messages
+    resources :messages, only: [:index, :create]
   end
 end
